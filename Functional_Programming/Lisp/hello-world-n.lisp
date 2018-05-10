271ba83862116b90ace1a3ddf932542adc0f8b99
Pedro Rodrigues <csixteen@gmail.com>
2018-05-10 18:51:56 +0100

    Trying when and progn instead of cond

--- Functional_Programming/Lisp/hello-world-n.lisp
+++ Functional_Programming/Lisp/hello-world-n.lisp
@@ -1,9 +1,9 @@
 (defun helloWorldNTimes ()
   (let ((n (parse-integer (read-line))))
     (labels ((phw (x)
-	     (cond ((= x 0) nil)
-		   (t (format t "Hello World~%")
-		      (phw (1- x))))))
+	     (when (> x 0)
+		   (progn (format t "Hello World~%") 
+			  (phw (1- x))))))
       (phw n))))
 
 (helloWorldNTimes)
