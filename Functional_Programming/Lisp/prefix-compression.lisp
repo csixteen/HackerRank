2fcdb8081fbd5c2a93c61f486cff57fff506000e
Pedro Rodrigues <csixteen@protonmail.com>
2018-05-21 21:39:37 +0100

    Final submission to Prefix Compression

--- Functional_Programming/Lisp/prefix-compression.lisp
+++ Functional_Programming/Lisp/prefix-compression.lisp
@@ -3,13 +3,13 @@
   (pref-comp (coerce x 'list) (coerce y 'list) ()))
 
 (defun pref-comp (x y prefix)
-  (cond ((or (and (null x) (null y))
+  (cond ((or (or (null x) (null y))
 	     (not (equal (car x) (car y))))
 	 (prefix-print x y prefix))
-	(t (pref-comp (cdr x) (cdr y) (append prefix (list (car x)))))))
+	(t (pref-comp (cdr x) (cdr y) (cons (car x) prefix)))))
 
 (defun prefix-print (x y prefix)
-  (format t "~d ~{~C~}~%" (length prefix) prefix)
+  (format t "~d ~{~C~}~%" (length prefix) (reverse prefix))
   (format t "~d ~{~C~}~%" (length x) x)
   (format t "~d ~{~C~}~%" (length y) y))
 
