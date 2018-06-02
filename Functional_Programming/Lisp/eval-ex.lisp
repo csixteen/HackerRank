01e402da77672d47fdf6673e00dc97dc12b911b8
Pedro Rodrigues <csixteen@protonmail.com>
2018-06-02 16:15:58 +0100

    Submission to Evaluating e^x

---
+++ Functional_Programming/Lisp/eval-ex.lisp
@@ -0,0 +1,13 @@
+;; https://www.hackerrank.com/challenges/eval-ex/problem
+(defun eval-ex (x)
+  (labels ((fact (n) (if (= n 1) 1 (* n (fact (1- n))))))
+    (+ 0 (loop for i
+	       from 1 to 9
+	       sum (/ (expt x i) (fact i))))))
+
+(defun solution (n)
+  (unless (<= n 0)
+    (format t "~,4f~%" (eval-ex (read)))
+    (solution (1- n))))
+
+(solution (read))
